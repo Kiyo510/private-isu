4a239f4d4bfd89faad334267e4bec78a9c8104f4
Kiyo510 <miraishida00510@gmail.com>
2024-11-17 20:29:16 +0900

    deploy shell private-isu

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+HOSTNAME:=$(shell hostname)
+BRANCH:=master
+
 .PHONY: init
 init: webapp/sql/dump.sql.bz2 benchmarker/userdata/img
 
@@ -12,3 +15,17 @@ benchmarker/userdata/img.zip:
 benchmarker/userdata/img: benchmarker/userdata/img.zip
 	cd benchmarker/userdata && \
 	unzip -qq -o img.zip
+
+.PHONY: deploy
+deploy: checkout start
+
+.PHONY: checkout
+checkout:
+	git fetch && \
+	git reset --hard origin/$(BRANCH)  && \
+	git switch -C $(BRANCH) origin/$(BRANCH)
+
+.PHONY: start
+start:
+	cd common && ./deploy.sh
+#	cd $(HOSTNAME) && ./deploy.sh
---
+++ common/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eux
+
+# 各種設定ファイルのコピー
+sudo cp -f etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp -f etc/nginx/nginx.conf /etc/nginx/nginx.conf
+#sudo cp -f etc/nginx/sites-available/isuconquest.conf /etc/nginx/sites-available/isuconquest.conf
+sudo nginx -t
+
+# アプリケーションのビルド
+cd /home/isucon/webapp/go
+go build -o isuconquest
+
+# ミドルウェア・Appの再起動
+# sudo systemctl restart mysql
+# sudo systemctl reload nginx
+
+# slow query logの有効化
+#QUERY="
+#set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
+#set global long_query_time = 0.01;
+#set global slow_query_log = ON;
+#"
+#echo $QUERY | sudo mysql -uroot
+
+# log permission
+sudo chmod -R 777 /var/log/nginx
+sudo chmod -R 777 /var/log/mysql
